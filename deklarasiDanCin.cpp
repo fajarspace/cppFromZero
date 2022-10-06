f01dd1a3df633e6e6bebcd7bdf998fb02b02446b
Fajar Agung <fajarspace@Fajars-MacBook-Pro.local>
2022-10-06 22:09:34 +0700

    add deklarasi dan cin

---
+++ deklarasiDanCin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a; //deklarasikan variabel
+    a = 10;
+    cout << a << endl;
+
+    int b; //deklarasikan variabel
+    cout << "masukkan nilai" << endl;
+    cin >> b; //cin, input nilai manual
+    cout << "nilai B adalah :";
+    cout << b << endl;
+}
