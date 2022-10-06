215710d93e604215f9434c277cd85e74643306ba
Fajar Agung <fajarspace@Fajars-MacBook-Pro.local>
2022-10-06 21:51:56 +0700

    Add variabel

---
+++ printingPadaConsole.cpp
@@ -0,0 +1,12 @@
+#include <iostream> //memanggil library iostream, menampilkan input output
+using namespace std;
+
+int main(){ //buat function main
+    cout << "Hello world " << endl; //endline
+    cout << "Hello nakama " << endl; //endline
+    cin.get(); //enter
+    return 0;
+}
+
+
+//satu baris disebut statement, setiap statement harus diakhiri dengan ;
---
+++ variabel.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 5; //integer, bilangan bulat
+
+    cin.get();
+    return 0;
+}
